9a1668821f758932567fb489f0dc19da67c108fc
Eliran Maimon and Thad Donaghue <eliran@>
2018-06-12 10:18:51 -0700

    build js functionality for create account, deposit, and withdraw

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,49 @@ function BankAccount(name, initialAmount) {
   // return this
 }
 
-BankAccount.prototype.deposit(amount) {
+BankAccount.prototype.deposit = function(amount) {
   this.balance += amount;
 }
 
-BankAccount.prototype.withdraw(amount) {
+BankAccount.prototype.withdraw = function(amount) {
   this.balance -= amount;
 }
 
-BankAccount.prototype.getBalance() {
+BankAccount.prototype.getBalance = function() {
   return this.balance;
 }
+
+$(document).ready(function() {
+  var accountArray = [];
+
+  $("#registration").submit(function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var initialAmount = parseFloat($("#initial-deposit").val());
+    var account = new BankAccount(name, initialAmount);
+    accountArray.push(account);
+    $("#current-balance").text("Your balance is $" + accountArray[0].getBalance().toFixed(2));
+    console.log(accountArray[0]);
+  });
+
+
+  $("#deposit-form").submit(function(event) {
+    event.preventDefault();
+    var depositAmount = parseFloat($("#deposit-amount").val());
+    accountArray[0].deposit(depositAmount);
+    $("#current-balance").text("Your balance is $" + accountArray[0].getBalance().toFixed(2));
+  });
+
+  $("#withdraw-form").submit(function(event) {
+    event.preventDefault();
+    var withdrawalAmount = parseFloat($("#withdrawal").val());
+    accountArray[0].withdraw(withdrawalAmount);
+    $("#current-balance").text("Your balance is $" + accountArray[0].getBalance().toFixed(2));
+  });
+
+
+
+
+
+
+});
