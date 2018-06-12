79759cd8601df19415b7247b011ec3f01b5145fe
Eliran Maimon and Thad Donaghue <thad@>
2018-06-12 14:39:34 -0700

    fix bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-function BankAccount(name, initialAmount) {
+function BankAccount(name, initialAmount, password) {
   // var this
   this.name = name;
   this.initialAmount = initialAmount;
   this.balance = initialAmount;
-  this.password = "";
+  this.password = password;
   // return this
 }
 
@@ -21,6 +21,10 @@ BankAccount.prototype.getBalance = function() {
 
 BankAccount.prototype.getName = function() {
   return this.name;
+}
+
+BankAccount.prototype.getPassword = function() {
+  return this.password;
 }
 
 function clearFields() {
@@ -41,12 +45,12 @@ function returnIndex(array, name) {
 
 $(document).ready(function() {
   var accountArray = [];
-  var name = "";
-  var returnIndex(
+  var arrIndex = 0;
 
   $("#registration").submit(function(event) {
     event.preventDefault();
     var name = $("#name").val();
+    var password = $("#create-password").val();
     var initialAmount = parseFloat($("#initial-deposit").val());
 
     if (name === "" || initialAmount === 0) {
@@ -54,31 +58,47 @@ $(document).ready(function() {
     }
 
     $("#home-collapse").hide().fadeIn(1500);
-    var account = new BankAccount(name, initialAmount);
+    var account = new BankAccount(name, initialAmount, password);
     accountArray.push(account);
-    $("#userName").text(accountArray[0].getName());
-    $("#current-balance").text("Your balance is $" + accountArray[0].getBalance().toFixed(2));
-    $("#current-accounts").append("<li>" + accountArray[0].getName() + "</li>");
-    $("#submit-button").attr("disabled", true);
+    $("#userName").text(accountArray[arrIndex].getName());
+    $("#current-balance").text("Your balance is $" + accountArray[arrIndex].getBalance().toFixed(2));
+    $("#current-accounts").append(accountArray[accountArray.length - 1].getName() + "</br>");
   });
 
 
   $("#deposit-form").submit(function(event) {
     event.preventDefault();
     var depositAmount = parseFloat($("#deposit-amount").val());
-    accountArray[0].deposit(depositAmount);
-    $("#current-balance").text("Your balance is $" + accountArray[0].getBalance().toFixed(2));
+    accountArray[arrIndex].deposit(depositAmount);
+    $("#current-balance").text("Your balance is $" + accountArray[arrIndex].getBalance().toFixed(2));
   });
 
   $("#withdraw-form").submit(function(event) {
     event.preventDefault();
     var withdrawalAmount = parseFloat($("#withdrawal").val());
-    accountArray[0].withdraw(withdrawalAmount);
-    $("#current-balance").text("Your balance is $" + accountArray[0].getBalance().toFixed(2));
+    if (isNaN(withdrawalAmount)) {
+      return;
+    }
+    accountArray[arrIndex].withdraw(withdrawalAmount);
+    $("#current-balance").text("Your balance is $" + accountArray[arrIndex].getBalance().toFixed(2));
   });
 
   $("#sign-out").click(function(){
     clearFields();
+  });
+
+  $("#login").submit(function(event) {
+    event.preventDefault();
+    var name = $("#loginName").val();
+    arrIndex = returnIndex(accountArray, name);
+    var enteredPassword = $("#password").val();
+    if (enteredPassword === accountArray[arrIndex].getPassword()) {
+      $("#home-collapse").show();
+      $("#wrong-password").text("");
+    } else {
+      $("#wrong-password").text("Incorrect username or password");
+    }
+
   });
 
 
